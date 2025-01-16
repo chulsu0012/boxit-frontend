cedeb9bd1a3cf94ad46abe0069a6c9487eff9fe2
[chulsu0012] <[chulsu0012@gmail.com]>
2025-01-17 03:16:07 +0900

    feat: add Landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './pages/Landing/index';
+import LoginPage from './pages/Login/index';
+import usePageStore from './store/pageStore';
+import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: React.FC = () => {
+  const currentPage = usePageStore((state) => state.currentPage);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to={currentPage} />} />
+          <Route path="/landing" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
