08ab989f6aec143a2119db7fe36378d0c0439098
[chulsu0012] <[chulsu0012@gmail.com]>
2025-01-19 00:02:53 +0900

    add: 레이아웃 개발용 임의의 이미지 파일

---
+++ frontend/src/data/TempPath.tsx
@@ -0,0 +1,7 @@
+// 백엔드와의 통신을 통해 가져와야 하는 임시 리소스
+
+import img1 from "../assets/temp/img01.jpg"
+import img2 from "../assets/temp/img02.jpg"
+import img3 from "../assets/temp/img03.jpg"
+
+export const images = [img1, img2, img3]
