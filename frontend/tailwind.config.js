22e78982d82ffd5d628e77110e2962eaab0d9917
[chulsu0012] <[chulsu0012@gmail.com]>
2025-01-18 17:30:47 +0900

    style: css 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        baseGray: '#F2F5F4'
+      }
+    },
   },
   plugins: [require('daisyui')],
 }
