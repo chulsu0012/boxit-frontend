47a533848b725970dd572b0ceff6d38272331494
[chulsu0012] <[chulsu0012@gmail.com]>
2025-01-19 00:05:44 +0900

    feat: 마우스 오버 여부에 따른 자동 슬라이드 기능을 위한 커스텀 훅

--- frontend/src/hooks/useInterval.ts
+++ frontend/src/hooks/useInterval.ts
@@ -1,8 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
-export const useInterval = (callback: () => void, duration: number = 4000) => {
+export const useInterval = (
+  callback: () => void,
+  duration: number = 4000,
+  handleOnHover: boolean = true
+) => {
+  const [isHover, setIsHover] = useState(false)
+
   useEffect(() => {
+    if (isHover) return
+
     const id = setInterval(callback, duration)
     return () => clearInterval(id)
-  }, [callback, duration])
+  }, [callback, duration, isHover])
+
+  return {
+    onMouseOver: () => handleOnHover && setIsHover(true),
+    onMouseLeave: () => handleOnHover && setIsHover(false)
+  }
 }
