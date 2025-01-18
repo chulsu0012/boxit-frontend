db382045f695eceabe5e10c59ed3de10866e9285
[chulsu0012] <[chulsu0012@gmail.com]>
2025-01-19 00:01:01 +0900

    style: 로고의 태그를 Div에서 Img로 변경, 복사 및 우클릭 금지

--- frontend/src/pages/Landing/Hero.tsx
+++ frontend/src/pages/Landing/Hero.tsx
@@ -2,22 +2,21 @@ import { Button } from "@/components/ui/Button"
 import { Div } from "@/components/ui/Div"
 import  logo  from "../../assets/react.svg"
 import { Link } from "react-router-dom"
+import { Img } from "@/components/ui/Img"
 
 export default function Hero() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Div>
-        <p className="text-2xl text-center">
+        <p className="text-2xl text-center select-none">
         매일매일 실천하는 하루 플래너
         </p>
       </Div>
-      <Div className="mt-4 mb-8">
-        <img
+      <Img
         src={logo}
         alt="BoxIt Logo"
-        className="w-[24rem] h-[9rem]"
-        />
-      </Div>
+        className="w-[24rem] h-[9rem] mt-4 mb-8"
+      />
       <Div>
         <Link to="/login">
           <Button className="h-16 text-2xl text-white bg-orange-500 rounded-lg hover:bg-orange-500 w-80">
